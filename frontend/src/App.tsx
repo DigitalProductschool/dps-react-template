b37c282a8ae43aacdaef0a2fc7253e28c377b01e
Nebo Milic <nebo.milic@gmail.com>
2024-02-14 10:44:14 +0100

    Add DPS logo, remove react and vite images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import dpsLogo from './assets/DPS.svg';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
-
+	const callHuggingface = () => {};
 	return (
 		<>
 			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
+				<a
+					href="https://https://www.digitalproductschool.io/"
+					target="_blank"
+				>
+					<img src={dpsLogo} className="logo" alt="DPS logo" />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
+			<h1>DPS + Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
+				<button onClick={() => alert('todo')}>
+					Call huggingface 🤗
 				</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
